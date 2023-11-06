d8b1b3e45870a261667a14912f14994c7000aa0f
Anukar <124155412+AnukarOP@users.noreply.github.com>
2023-11-06 19:46:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="text-center py-6 font-medium">To-do list</footer>;
+  return (
+    <footer className="text-center py-6 font-medium">
+      <span style={{ fontWeight: "bold" }}>ManageitUP</span> -{" "}
+      <a href="https://Anukarop.is-a.dev" target="_blank" rel="noopener noreferrer">
+        AnukarOP
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
